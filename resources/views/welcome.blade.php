2d586e509181c8e6e6685061f99cd42579025ffb
Faundez21 <cfaundezs@it.ucsc.cl>
2024-11-15 15:36:22 -0300

    dsfdsfs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                   Pablo is a Gay Bpy
+                                   Pablo dfd
                                 </div>
                             </div>
                         </div>
